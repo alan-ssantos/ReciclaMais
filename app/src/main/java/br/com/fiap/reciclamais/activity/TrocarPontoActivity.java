3455a80e78dfbfc98f381accafab87fa2e11c3d5
Felipe Nakata Figueiredo <felipe.figueiredo@ibm.com>
2019-10-21 18:34:17 -0300

    Nova tela de voucher

---
+++ app/src/main/java/br/com/fiap/reciclamais/activity/TrocarPontoActivity.java
@@ -0,0 +1,20 @@
+package br.com.fiap.reciclamais.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import br.com.fiap.reciclamais.R;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class TrocarPontoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trocar_ponto);
+    }
+
+
+    public void trocarPontos(View view) {
+    }
+}
